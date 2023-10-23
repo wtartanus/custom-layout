c93b1fa1edc2436120c7a089a2105762e248b3cb
Wojciech Tartanus <wojciech.tartanus@synergycodes.com>
2023-10-23 17:48:20 +0200

    Node templates

--- src/gojs/diagram/diagram.ts
+++ src/gojs/diagram/diagram.ts
@@ -5,6 +5,24 @@ const $ = go.GraphObject.make;
 export const createDiagram = (diagramDiv: HTMLDivElement) => {
     const diagram = $(go.Diagram, diagramDiv);
 
+    diagram.nodeTemplateMap = new go.Map([
+        { key: '', value: $(
+            go.Node,
+            $(
+                go.Panel,
+                go.Panel.Vertical,
+                $(
+                    go.Shape,
+                    'Rectangle',
+                    {
+                        fill: 'orange',
+                        desiredSize: new go.Size(100, 100)
+                    }
+                ),
+            )
+        ) },
+    ]);
+
     (window as any).goJsDiagram = diagram;
     
     return diagram;
--- src/gojs/palette/palette.ts
+++ src/gojs/palette/palette.ts
@@ -5,5 +5,25 @@ const $ = go.GraphObject.make;
 export const createPalette = (paletteDiv: HTMLDivElement) => {
     const palette = $(go.Palette, paletteDiv);
 
+    palette.nodeTemplateMap = new go.Map([
+        { key: '', value: $(
+            go.Node,
+            $(
+                go.Panel,
+                go.Panel.Auto,
+                $(
+                    go.Shape,
+                    'Rectangle',
+                    {
+                        fill: 'orange',
+                        desiredSize: new go.Size(100, 100)
+                    }
+                ),
+            )
+        ) },
+    ]);
+
+    palette.model.nodeDataArray = [{}];
+
     palette.padding = 20;
 }
